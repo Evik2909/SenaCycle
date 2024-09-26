947c299b9d9dfd53eca42f12cb97bc11679e7c55
Luis-Lucuara <lucuara710@gmail.com>
2024-09-26 13:55:06 -0500

    Add files via upload

--- controllers/ceve.php
+++ controllers/ceve.php
@@ -1,30 +1,61 @@
 <?php
 include "models/meve.php";
 
+
+// Se define y/o llena la variable de ope para saber que operacion se va a realizar en la tabla de 'alquiler'
+$ope = isset($_REQUEST['ope']) ? $_REQUEST['ope'] : null;
+
 $ideve = isset($_REQUEST['ideve']) ? $_REQUEST['ideve'] : null;
 $nomeve = isset($_POST['nomeve']) ? $_POST['nomeve'] : null;
 $deseve = isset($_POST['deseve']) ? $_POST['deseve'] : null;
 $feceve = isset($_POST['feceve']) ? $_POST['feceve'] : null;
-$idusu = $_SESSION['idusu'];
-$ope = isset($_REQUEST['ope']) ? $_REQUEST['ope'] : null;
+$idusu = isset($_GET['idusu']) ? $_GET['idusu'] : null;
+
 //Instanciar el modelo:
 $meve = new Meve();
-$meve->setIdeve($ideve);
-if ($ope == "save") {
-    $meve->setNomeve($nomeve);
-    $meve->setDeseve($deseve);
-    $meve->setFeceve($feceve);
-    $meve->setIdusu($idusu);
-    if($ideve)$meve->edit($ideve);
-    else $meve->save(); 
-} 
-if ($ope=="del" && $ideve)$meve->delete($ideve);
-    if ($ope=="edi" && $ideve){
-        $dtOne = $meve->getOne($ideve); 
-    }else{ 
-        $dtOne=NULL;
-    };
+
+
+//Se comprueba que todos los datos que vamos a incertar en la tabla vengan llenos o definidos:
+if (!empty($ideve) && !empty($nomeve) && !empty($deseve) && !empty($feceve) && !empty($idusu)) {
+
+    if (!empty($ope) && $ope == "save") {
+
+        //Se envian todos los datos al modelo 'meve'
+        $meve->setIdeve($ideve);
+        $meve->setNomeve($nomeve);
+        $meve->setDeseve($deseve);
+        $meve->setFeceve($feceve);
+        $meve->setIdusu($idusu);
+
+        //Se ejecuta la consulta en la base de datos
+        $meve->save();
+
+    } elseif (!empty($ope) && $ope == "edit") {
+
+        $meve->setIdeve($ideve);
+        $meve->setNomeve($nomeve);
+        $meve->setDeseve($deseve);
+        $meve->setFeceve($feceve);
+        $meve->setIdusu($idusu);
+        //Se ejecuta la consulta en la base de datos
+        $meve->edit($ideve);
+
+    }elseif(!empty($ope) && $ope == "delete"){
+        $meve->setIdeve($ideve);
+        $meve->delete($ideve);
+    }else{
+        echo "<script>alert('ERROR: Operación no válida')</script>";
+    }
+    //Se envian los datos
+
+}
 
 // Obtener todos los datos de los eventos y guardalos en una variable
-$dtEve = $meve->getAll();
+$dtEventos = $meve->getAll();
+
+//Conteo de los eventos
+$totEventos = count($dtEventos);
+
+//Obtener todos los eventos de un usuario en especifico
+$dtEventosxusuario = $meve->getEventosxusuario($_SESSION['idusu']);
 
